f0737248044fbbede712a651e4e5f5f3827f4a0d
DESKTOP-BTTJJGD\4evaj <4evajsh@gmail.com>
2021-10-30 21:39:01 +0900

    create project

---
+++ src/main/java/kr/couchcoding/tennis_together_crawler/TennisTogetherCrawlerApplication.java
@@ -0,0 +1,13 @@
+package kr.couchcoding.tennis_together_crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisTogetherCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisTogetherCrawlerApplication.class, args);
+	}
+
+}
